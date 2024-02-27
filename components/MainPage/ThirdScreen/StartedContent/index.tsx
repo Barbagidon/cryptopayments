70dc4bfa486e99bc6fc4be6e58de212ee0039a2f
Den Shtiller <77959708+Barbagidon@users.noreply.github.com>
2024-02-27 20:04:03 +0300

    third section completed

--- components/MainPage/ThirdScreen/StartedContent/index.tsx
+++ components/MainPage/ThirdScreen/StartedContent/index.tsx
@@ -5,11 +5,13 @@ import StartedBtn from "@/components/ui/StartedBtn";
 const StartedContent = () => {
   return (
     <div className={styles.startedContent}>
-      <div className={styles.title}>Unlock the Power of Crypto:</div>
-      <div className={styles.descr}>
-        Integrate Cryptocurrency Payments into Your Business Effortlessly!
+      <div className={styles.textWrap}>
+        <div className={styles.title}>Unlock the Power of Crypto:</div>
+        <div className={styles.descr}>
+          Integrate Cryptocurrency Payments into Your Business Effortlessly!
+        </div>
       </div>
-      <StartedBtn />
+      <StartedBtn className={styles.startedBtn} />
     </div>
   );
 };
