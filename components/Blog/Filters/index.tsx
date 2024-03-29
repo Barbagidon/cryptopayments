1c13a4c411da50c8faafe48440216a0840d65b27
Den Shtiller <77959708+Barbagidon@users.noreply.github.com>
2024-03-30 01:15:47 +0300

    added config for filters

--- components/Blog/Filters/index.tsx
+++ components/Blog/Filters/index.tsx
@@ -2,32 +2,7 @@ import React from "react";
 import styles from "./styles.module.css";
 import cn from "classnames";
 import Link from "next/link";
-
-const filters = [
-  {
-    title: "All",
-    href: "/blog",
-  },
-  {
-    title: "Events",
-    href: "/blog?filter=events",
-  },
-
-  {
-    title: "News",
-    href: "/blog?filter=news",
-  },
-
-  {
-    title: "Explainers",
-    href: "/blog?filter=explainers",
-  },
-
-  {
-    title: "Articles",
-    href: "/blog?filter=articles",
-  },
-];
+import { filters } from "./config";
 
 const Filters = () => {
   return (
