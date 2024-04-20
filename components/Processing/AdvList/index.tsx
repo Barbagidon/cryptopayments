ee3b902d2530e7c59a447f5f6fdc80bafb6efbc1
Den Shtiller <77959708+Barbagidon@users.noreply.github.com>
2024-04-20 12:07:20 +0300

    completed animate in adv list section

--- components/Processing/AdvList/index.tsx
+++ components/Processing/AdvList/index.tsx
@@ -32,96 +32,23 @@ const AdvList = () => {
     amount: 0.5,
   });
 
-  const transition = { duration: 0.7, ease: "easeOut" };
-
   return (
     <SectionWrap>
       <div ref={containerRef} className={styles.content}>
         <motion.div
           className={cn(styles.advList, { [styles.animAdvList]: isInView })}
         >
-          {/* {config.map((item, i) => {
+          {config.map((item, i) => {
             return (
-              <motion.div key={i}>
-                <AdvCard
-                  // className={cn(styles.advItem, {
-                  //   [styles.advItemAnim]: isInView,
-                  // })}
-                  cardData={item}
-                />
-              </motion.div>
+              <AdvCard
+                key={i}
+                className={cn(styles.animWrap, {
+                  [styles.showAnim]: isInView,
+                })}
+                cardData={item}
+              />
             );
-          })} */}
-
-          <motion.div
-            className={cn(styles.animWrap, { [styles.showAnim]: isInView })}
-            // style={{
-            //   position: "relative",
-            //   bottom: -526,
-            // }}
-            // animate={{
-            //   bottom: isInView ? 0 : -526,
-            // }}
-            // initial={{
-            //   bottom: -526,
-            // }}
-            // whileInView={{
-            //   bottom: 0,
-            // }}
-            transition={transition}
-          >
-            <AdvCard
-              // className={cn(styles.advItem, {
-              //   [styles.advItemAnim]: isInView,
-              // })}
-              cardData={config[0]}
-            />
-          </motion.div>
-          <motion.div
-            className={cn(styles.animWrap, { [styles.showAnim]: isInView })}
-            // style={{
-            //   position: "relative",
-            //   bottom: -357,
-            // }}
-            // initial={{ bottom: -357 }}
-            // whileInView={{
-            //   bottom: 0,
-            // }}
-
-            // animate={{ bottom: isInView ? 0 : -357 }}
-            transition={transition}
-          >
-            <AdvCard
-              // className={cn(styles.advItem, {
-              //   [styles.advItemAnim]: isInView,
-              // })}
-              cardData={config[1]}
-            />
-          </motion.div>
-          <motion.div
-            className={cn(styles.animWrap, { [styles.showAnim]: isInView })}
-            // style={{
-            //   position: "relative",
-            //   bottom: -189,
-            // }}
-            // animate={{
-            //   bottom: isInView ? 0 : -189,
-            // }}
-            // initial={{
-            //   bottom: -189,
-            // }}
-            // whileInView={{
-            //   bottom: 0,
-            // }}
-            transition={transition}
-          >
-            <AdvCard
-              // className={cn(styles.advItem, {
-              //   [styles.advItemAnim]: isInView,
-              // })}
-              cardData={config[2]}
-            />
-          </motion.div>
+          })}
         </motion.div>
       </div>
     </SectionWrap>
