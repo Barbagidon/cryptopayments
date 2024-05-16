2998d8d9cfff1b048872a4f6ac87c76765a04bf8
Den Shtiller <77959708+Barbagidon@users.noreply.github.com>
2024-05-16 21:08:58 +0300

    half second screen completed

--- components/ui/BigCircle/index.tsx
+++ components/ui/BigCircle/index.tsx
@@ -1,16 +1,29 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import styles from "./styles.module.css";
 import cn from "classnames";
+import { useTheme } from "@/stores/useTheme";
 interface Props {
   className?: string;
 }
 
 const BigCircle = ({ className }: Props) => {
+  const theme = useTheme((state) => state.theme);
+
+  const src = theme === "light" ? "/gear.png" : "/gearDark.png";
+
   return (
     <div className={cn(styles.bigCircle)}>
       <div className={styles.gearIconWrap}>
-        <Image className={className} alt={"gear icon"} fill src={"/gear.png"} />
+        <Image
+          priority
+          className={className}
+          alt={"gear icon"}
+          fill
+          src={src}
+        />
       </div>
     </div>
   );
