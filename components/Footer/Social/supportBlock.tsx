ccac554fdbeab2211f8d47835b96aa3dbab0bed4
BorisSlonov <sslonovborisss@gmail.com>
2024-05-17 12:39:00 +0300

    dark theme footer

---
+++ components/Footer/Social/supportBlock.tsx
@@ -0,0 +1,33 @@
+'use client'
+import styles from "./styles.module.css"
+import FooterTitle from "../FooterTitle";
+import Image from "next/image";
+import Arrow from "./icons/arrow";
+import { useTheme } from "@/stores/useTheme";
+
+
+function SupportBlock() {
+    const theme = useTheme((state) => state.theme);
+
+    const src =
+        theme === "light"
+            ? "/footerSocialBg.png"
+            : "/darkFooterSocialBg.png";
+    return (
+        <div className={styles.supportBlock}>
+            <FooterTitle>Support:</FooterTitle>
+            <span className={styles.link}>support@cryptopayments.com</span>
+            <div className={styles.logo}>
+                <Image
+                    src={src}
+                    alt="logo"
+                    width={74}
+                    height={74}
+                />
+            </div>
+            <Arrow className={styles.arrow} />
+        </div>
+    )
+}
+
+export default SupportBlock;
