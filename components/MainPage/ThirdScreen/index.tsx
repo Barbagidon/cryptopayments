5e21b23c951ed62e8158160d52f2aff5a1d56e92
Den Shtiller <77959708+Barbagidon@users.noreply.github.com>
2024-05-17 23:00:38 +0300

    changed images to svg && cahnge amount for anim  in exchange section

--- components/MainPage/ThirdScreen/index.tsx
+++ components/MainPage/ThirdScreen/index.tsx
@@ -7,6 +7,7 @@ import StartedContent from "./StartedContent";
 import InstructionList from "./InstructionList";
 import { useInView } from "framer-motion";
 import cn from "classnames";
+import RoundTextIcon from "./icons/roundTextIcon";
 
 const ThirdScreen = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -15,7 +16,11 @@ const ThirdScreen = () => {
     <section ref={containerRef} className={styles.thirdScreen}>
       <div className={styles.content}>
         <div className={styles.header}>
-          <div className={cn(styles.titleWrap, { [styles.titleWrap_active]: isInView })} >
+          <div
+            className={cn(styles.titleWrap, {
+              [styles.titleWrap_active]: isInView,
+            })}
+          >
             <h2 className={styles.title}>Fast integration</h2>
             <div className={styles.titleDivider} />
             <div className={styles.triangleWrap}>
@@ -23,12 +28,12 @@ const ThirdScreen = () => {
             </div>
           </div>
 
-          <div className={cn(styles.imageWrap, { [styles.imageWrap_active]: isInView })}>
-            <Image
-              fill
-              src={"/mainPage/thirdSectionText.png"}
-              alt={"round text"}
-            />
+          <div
+            className={cn(styles.imageWrap, {
+              [styles.imageWrap_active]: isInView,
+            })}
+          >
+            <RoundTextIcon />
           </div>
         </div>
         <div className={styles.bottomContent}>
