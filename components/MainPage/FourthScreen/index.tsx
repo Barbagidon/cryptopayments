abb8d18451a0f50415d195c4ef90fab0bddf110e
Den Shtiller <77959708+Barbagidon@users.noreply.github.com>
2024-05-17 20:29:53 +0300

    completed fourth section

--- components/MainPage/FourthScreen/index.tsx
+++ components/MainPage/FourthScreen/index.tsx
@@ -8,18 +8,26 @@ import Bg from "./icons/bg";
 
 import { useInView } from "framer-motion";
 import cn from "classnames";
-
+import { useTheme } from "@/stores/useTheme";
 
 const FourthScreen = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const isInView = useInView(containerRef, { once: true });
-  const isHalfInView = useInView(containerRef, { once: true, amount: .4 });
+  const isHalfInView = useInView(containerRef, { once: true, amount: 0.4 });
+  const theme = useTheme((state) => state.theme);
+
+  const coinsSrc = theme === "light" ? "/coins.png" : "/coinsDark.png";
+
   return (
     <section ref={containerRef} className={styles.fourthScreen}>
       <div className={styles.content}>
         <div className={styles.topContent}>
           <div className={styles.topText}>
-            <div className={cn(styles.titleWrap, { [styles.titleWrap_active]: isInView })}>
+            <div
+              className={cn(styles.titleWrap, {
+                [styles.titleWrap_active]: isInView,
+              })}
+            >
               <h2 className={styles.title}>Multicurrency support</h2>
               <div className={styles.descr}>
                 Say Goodbye to traditional payment barriers and Hello to the
@@ -28,7 +36,11 @@ const FourthScreen = () => {
             </div>
           </div>
 
-          <div className={cn(styles.titleWrap, { [styles.titleWrap_active]: isInView })}>
+          <div
+            className={cn(styles.titleWrap, {
+              [styles.titleWrap_active]: isInView,
+            })}
+          >
             <StartedBtn className={styles.startedBtn} />
           </div>
         </div>
@@ -44,10 +56,12 @@ const FourthScreen = () => {
           <Image
             width={0}
             height={0}
-            className={cn(styles.coinsImg, { [styles.coinsImg_active]: isHalfInView })}
+            className={cn(styles.coinsImg, {
+              [styles.coinsImg_active]: isHalfInView,
+            })}
             priority
             unoptimized
-            src={"/coins.png"}
+            src={coinsSrc}
             alt="coin image"
           />
         </div>
